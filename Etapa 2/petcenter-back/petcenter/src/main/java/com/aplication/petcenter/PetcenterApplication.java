395b2a9940e371fac4ab7e35fa06ddf82f678236
Letícia <32845746+leticiafernanda@users.noreply.github.com>
2022-06-08 23:55:26 -0300

    API
    configurações iniciais da api

---
+++ Etapa 2/petcenter-back/petcenter/src/main/java/com/aplication/petcenter/PetcenterApplication.java
@@ -0,0 +1,13 @@
+package com.aplication.petcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcenterApplication.class, args);
+	}
+
+}
