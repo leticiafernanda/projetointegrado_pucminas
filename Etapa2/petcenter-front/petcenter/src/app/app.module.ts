05468dfda1d63ea379d16acb68fb6c63348c7aaf
Letícia <32845746+leticiafernanda@users.noreply.github.com>
2022-07-02 18:24:16 -0300

    sccs e themes

--- Etapa2/petcenter-front/petcenter/src/app/app.module.ts
+++ Etapa2/petcenter-front/petcenter/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
