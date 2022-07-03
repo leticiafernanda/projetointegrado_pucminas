64ff6619a085f4510af1d3f79fd9db578cb2a1b3
Letícia <32845746+leticiafernanda@users.noreply.github.com>
2022-07-03 18:23:29 -0300

    ajustes service

--- Etapa2/petcenter-back/petcenter/src/main/java/com/aplication/petcenter/service/impl/AdministradorImplService.java
+++ Etapa2/petcenter-back/petcenter/src/main/java/com/aplication/petcenter/service/impl/AdministradorImplService.java
@@ -3,9 +3,14 @@ package com.aplication.petcenter.service.impl;
 import com.aplication.petcenter.domain.dto.AdministradorDTO;
 import com.aplication.petcenter.domain.entity.Administrador;
 import com.aplication.petcenter.service.AdministradorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
-
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class AdministradorImplService implements AdministradorService {
 
     @Override
