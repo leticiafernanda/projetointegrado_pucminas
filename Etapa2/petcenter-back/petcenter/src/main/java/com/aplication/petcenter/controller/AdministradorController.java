090708eea908084ba555fed8e50e6e410ff2da77
Letícia <32845746+leticiafernanda@users.noreply.github.com>
2022-07-03 18:12:47 -0300

    ajustes AdministradorController e AdministradorImplService

--- Etapa2/petcenter-back/petcenter/src/main/java/com/aplication/petcenter/controller/AdministradorController.java
+++ Etapa2/petcenter-back/petcenter/src/main/java/com/aplication/petcenter/controller/AdministradorController.java
@@ -1,10 +1,15 @@
 package com.aplication.petcenter.controller;
 
+import com.aplication.petcenter.domain.dto.AdministradorDTO;
+import com.aplication.petcenter.domain.entity.Administrador;
 import com.aplication.petcenter.service.AdministradorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/adm")
@@ -13,4 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdministradorController {
 
     private final AdministradorService administradorService;
+
+    @GetMapping
+    public ResponseEntity<List<AdministradorDTO>> findAdministradorList() {
+        List<AdministradorDTO> retorno = administradorService.findClienteList();
+        return ResponseEntity.ok(retorno);
+    }
+    @DeleteMapping("/{id_administrador}")
+    public ResponseEntity<Void> deleteAdministrados(@PathVariable(value = "id_administrador") Integer admId) {
+        administradorService.deleteById(admId);
+        return ResponseEntity.noContent().build();
+    }
+    @PostMapping
+    public ResponseEntity<Void> createAdmin(@RequestBody Administrador adm) {
+        administradorService.save(adm);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 }
---
+++ Etapa2/petcenter-back/petcenter/src/main/java/com/aplication/petcenter/service/impl/AdministradorImplService.java
@@ -0,0 +1,25 @@
+package com.aplication.petcenter.service.impl;
+
+import com.aplication.petcenter.domain.dto.AdministradorDTO;
+import com.aplication.petcenter.domain.entity.Administrador;
+import com.aplication.petcenter.service.AdministradorService;
+
+import java.util.List;
+
+public class AdministradorImplService implements AdministradorService {
+
+    @Override
+    public List<AdministradorDTO> findClienteList() {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Integer admId) {
+
+    }
+
+    @Override
+    public void save(Administrador adm) {
+
+    }
+}
