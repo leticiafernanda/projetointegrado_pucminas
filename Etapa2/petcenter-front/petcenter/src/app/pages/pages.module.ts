958607b42fe035ea22791ca4e042fb024e70f388
Letícia <32845746+leticiafernanda@users.noreply.github.com>
2022-07-21 22:59:34 -0300

    correções no css, chamada no services

--- Etapa2/petcenter-front/petcenter/src/app/pages/pages.module.ts
+++ Etapa2/petcenter-front/petcenter/src/app/pages/pages.module.ts
@@ -6,11 +6,12 @@ import { MaterialModule } from "../material.module";
 import { HomeComponent } from './home/home.component';
 import { CoreModule } from "../core/core.module";
 import { RouterModule } from "@angular/router";
-import { VisualizarConsultaComponent } from './visualizar-consulta/visualizar-consulta.component';
+import { VisualizarConsultaComponent } from './consulta/visualizar-consulta/visualizar-consulta.component';
+import { ConsultaComponent } from './consulta/consulta.component';
 
 
 @NgModule({
-  declarations: [ LoginPageComponent, HomeComponent, VisualizarConsultaComponent],
+  declarations: [ LoginPageComponent, HomeComponent, VisualizarConsultaComponent, ConsultaComponent],
   imports: [CommonModule,  FormsModule,MaterialModule,CoreModule,RouterModule.forChild([])],
   exports: [ LoginPageComponent]
 })
