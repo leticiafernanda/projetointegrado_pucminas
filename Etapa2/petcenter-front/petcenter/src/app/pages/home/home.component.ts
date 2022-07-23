97fdf85a5762dfeb89b9686cae1363f78190a033
Letícia <32845746+leticiafernanda@users.noreply.github.com>
2022-07-23 15:38:52 -0300

    alterações do layout da tela inicial

--- Etapa2/petcenter-front/petcenter/src/app/pages/home/home.component.ts
+++ Etapa2/petcenter-front/petcenter/src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router,  private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
-  longText = ``;
+  handleClickVisualizarConsulta(){
+    this.router.navigate([`/visualizar-consulta`])
+  }
 }
