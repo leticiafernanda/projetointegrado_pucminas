52ced66ac60fbb0da43a7bb1a1cd8567f3ac3dda
Letícia <leticia.santos@dtidigital.com.br>
2022-10-04 22:01:34 -0300

    ajustes home

--- Etapa3/petcenter-front/petcenter/src/app/pages/home/home.component.ts
+++ Etapa3/petcenter-front/petcenter/src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +8,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public router: Router,  private route: ActivatedRoute) { }
+  constructor(public router: Router) { }
 
   ngOnInit(): void {
   }
